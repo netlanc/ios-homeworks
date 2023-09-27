24ffcd5615ed5c91817cfbd5e3dfeb841987bb61
netlanc <netlanc@yandex.ru>
2023-09-27 21:10:34 +0300

    Items 1 to 5 have been completed

---
+++ Navigation/FeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by netlanc on 27.09.2023.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by netlanc on 27.09.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
